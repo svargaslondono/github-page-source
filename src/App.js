d6de0ad5d85319480b865076db024040dabf03fa
Sebastian Vargas L <svargaslondono@gmail.com>
2022-06-06 19:47:14 -0500

    Include basic initial layout for site.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import profilePic from "./prof-1.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Hi! My name is Sebastian.</h1>
+
+      <div className="App-description">
+        <p>I'm a web developer with a keen eye for detail.</p>
         <p>
-          Site under construction.
+          Check out my{" "}
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/svargaslondono/">
+            LinkedIn profile
+          </a>{" "}
+          and my{" "}
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/svargaslondono">
+            GitHub repos
+          </a>
         </p>
-      </header>
+        <img src={profilePic} className="App-logo" alt="logo" />
+      </div>
+
+      <footer className="App-footer">
+        <em>
+          {"\u27C1"} This site is under construction. A lot more love is being
+          added. {"\u2665"}
+        </em>
+      </footer>
     </div>
   );
 }
